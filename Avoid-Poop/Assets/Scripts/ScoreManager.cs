3cd98473863cbf6150beed5486a525872b9c1735
CHICHI <smartb12@naver.com>
2024-05-17 11:25:51 +0900

    ScoreManager

---
+++ Avoid-Poop/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text currentScoreTxt;
+    [SerializeField] TMP_Text highScoreText;
+
+    private float score = 0;
+    private float bestScore = 0;
+    private string key = "BestScore";
+
+    private void Awake()
+    {
+        bestScore = PlayerPrefs.GetFloat(key, 0);
+        highScoreText.text = $"HIGH : {bestScore.ToString("N2")}";
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score();
+    }
+
+    public void Score()
+    {
+        score = Time.time;
+        currentScoreTxt.text = $"Score : {score.ToString("N2")}";
+
+        if (score > bestScore)
+        {
+            PlayerPrefs.SetFloat(key, score);
+        }
+    }
+}
