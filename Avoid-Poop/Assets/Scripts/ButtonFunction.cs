b825a1e73a8511fd3674814396a5f88fed7b4d56
Kwonyougsu <ysun5311@gmail.com>
2024-05-17 12:38:29 +0900

    SoundSetting

---
+++ Avoid-Poop/Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonFunction : MonoBehaviour
+{
+    public GameObject SoundSetting;
+    public void StartBtn()
+    {
+        //메인 씬으로 이동 
+        SceneManager.LoadScene(1);
+    }
+    public void Soundsetting()
+    {
+        SoundSetting.SetActive(true);
+    }
+
+    public void AudioOnToggle()
+    {
+        AudioListener.volume = 1.0f;
+    }
+    public void AudioOffToggle()
+    {
+        AudioListener.volume = 0.0f;
+    }
+    public void Close()
+    {
+        SoundSetting.SetActive(false);
+    }
+}
