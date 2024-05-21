317f4593f4ba84b77d187aef92579e1d27550e0b
Kwonyougsu <ysun5311@gmail.com>
2024-05-21 16:05:58 +0900

    난이도 버튼 싱글톤 수정

--- Avoid-Poop/Assets/Scripts/UI/DiffManager.cs
+++ Avoid-Poop/Assets/Scripts/UI/DiffManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DiffManager : MonoBehaviour
 {
     public static DiffManager Instance;
 
     //난이도 입력 값 
-    private float difficultyInterval; 
+    private float difficultyInterval;
 
     private void Awake()
     {
+        Time.timeScale = 1.0f;
         if (Instance == null)
         {
             Instance = this;
@@ -23,11 +25,13 @@ public class DiffManager : MonoBehaviour
         }
     }
 
+    //다시 start 씬을 로드할 때, 옵젝이 2개가 되어버림. -> 싱글톤은 직접적으로 넣는것이 아니다. 기능을 끌어다쓴다.
     public void SetDifficulty(float interval)
     {
         difficultyInterval = interval;
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("MainScene");
     }
+    
 
     public float GetDifficulty()
     {
