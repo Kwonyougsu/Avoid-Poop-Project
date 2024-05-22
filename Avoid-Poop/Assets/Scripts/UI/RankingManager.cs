c0caffa314692684a1c14345fda40ef1f6e24b47
CHICHI <smartb12@naver.com>
2024-05-22 16:50:34 +0900

    RankingManager.cs 완성

--- Avoid-Poop/Assets/Scripts/UI/RankingManager.cs
+++ Avoid-Poop/Assets/Scripts/UI/RankingManager.cs
@@ -39,6 +39,12 @@ public class RankingManager : MonoBehaviour
         UpdateRankText();
         RankingSet(0, "Unknown");
     }
+   
+    [ContextMenu("Reset")]
+    public void ResetPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
 
     private void UpdateRankText()
     {
@@ -60,22 +66,21 @@ public class RankingManager : MonoBehaviour
         // 1위부터 아래로 검사를 하고 만약에 더 높은 점수가 나오면 반복문 돌리면서 1번에서 찾았다고 하면 아래서부터 땡기기
         // 5위부터 위로 변경
         // 현재 점수가 5위 점수보다 높을 시
-        float tempScore;
 
-        if (currentScore > bestScores[4])
+        for (int i = 0; i < 5; i++)
         {
-            // 5위를 현재 점수로 바꾼다
-            bestScores[4] = currentScore;
-            for (int i = 4; i > 0; i--)
+            if (currentScore > bestScores[i])
             {
-                if (bestScores[i] > bestScores[i - 1])
+                // 새로운 점수가 기존 점수보다 높을 경우 삽입
+                for (int j = 4; j > i; j--)
                 {
-                    bestScores[i - 1] = bestScores[i];
+                    bestScores[j] = bestScores[j - 1];
+                    bestName[j] = bestName[j - 1];
                 }
-            }
-            if (currentScore < bestScores[0])
-            {
-                bestScores[0] = currentScore;
+
+                bestScores[i] = currentScore;
+                bestName[i] = currentName;
+                break;
             }
         }
 
