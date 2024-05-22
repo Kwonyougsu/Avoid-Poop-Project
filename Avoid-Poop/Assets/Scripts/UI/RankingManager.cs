352ac264ff2ef6d4a99d8d10187a9ff958752f0b
CHICHI <smartb12@naver.com>
2024-05-22 15:33:33 +0900

    ranking

--- Avoid-Poop/Assets/Scripts/UI/RankingManager.cs
+++ Avoid-Poop/Assets/Scripts/UI/RankingManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class RankingManager : MonoBehaviour
 {
-    public ScoreManager scoreManager;
+    private ScoreManager scoreManager;
 
     [SerializeField] TMP_Text Ranker1;
     [SerializeField] TMP_Text Ranker2;
@@ -19,77 +19,76 @@ public class RankingManager : MonoBehaviour
 
     private float[] bestScores = new float[5];
     private string[] bestName = new string[5];
-    private float[] rankScore = new float[5];
-    private string[] rankName = new string[5];
     private float nowScore;
     private string playerName;
 
     private void Start()
     {
-        //Ranker1.text = $"{rankName[0]} : {rankScore[0]}";
-        //Ranker2.text = $"{rankName[1]} : {rankScore[1]}";
-        //Ranker3.text = $"{rankName[2]} : {rankScore[2]}";
-        //Ranker4.text = $"{rankName[3]} : {rankScore[3]}";
-        //Ranker5.text = $"{rankName[4]} : {rankScore[4]}";
-    }
-
-       
-
-    // 현재 플레이어의 점수와 이름을 받아서 실행됨
-    private void RankingSet(float currentScore, string currentName)
-    {
-        int j = 0;
-
-        // 현재에 저장하고 시작
-        PlayerPrefs.SetString(j.ToString() + "BestName", currentName);
-        PlayerPrefs.SetFloat(j.ToString() + "BestScore", currentScore);
-
-        float score = 0f;
-        string Name = "";
+        // 초기 값이 없으면 기본 값 설정
 
         for (int i = 0; i < 5; i++)
         {
-            // 저장된 최고점수와 이름 가져오기
+            if (!PlayerPrefs.HasKey(i.ToString() + "BestScore"))
+            {
+                PlayerPrefs.SetFloat(i.ToString() + "BestScore", 0f);
+                PlayerPrefs.SetString(i.ToString() + "BestName", "Unknown");
+            }
             bestScores[i] = PlayerPrefs.GetFloat(i.ToString() + "BestScore");
             bestName[i] = PlayerPrefs.GetString(i.ToString() + "BestName");
-
-            // 현재 점수가 랭킹에 오를 수 있다면
-            if (bestScores[i] < currentScore)
-            {
-                // 스왑한다 ( 현재 랭킹에 있던 점수를 빼내고 빈 자리에 현재 점수를 넣는다. 자리 바꾸기 )
-                score = bestScores[i];
-                Name = bestName[i];
-                bestScores[i] = currentScore;
-                bestName[i] = currentName;
-
-                // 랭킹에 저장
-                PlayerPrefs.SetFloat(i.ToString() + "BestScore", currentScore);
-                PlayerPrefs.SetString(i.ToString() + "BestName", currentName);
-
-                // 다음 반복을 위해서
-                currentScore = score;
-                currentName = name;
-            }
-
-            Debug.Log($"{bestName[0]}");
-            Debug.Log($"{bestScores[0]}");
         }
-        // 랭킹에 맞춰서 점수와 이름을 저장한다.
-        Array.Sort(bestScores);
-        Array.Sort(bestName);
+        UpdateRankText();
+        RankingSet(0, "Unknown");
+    }
 
-        for (int i = 0; i < 5; i++)
+    private void UpdateRankText()
+    {
+        Ranker1.text = FormatRankText(bestName[0], bestScores[0]);
+        Ranker2.text = FormatRankText(bestName[1], bestScores[1]);
+        Ranker3.text = FormatRankText(bestName[2], bestScores[2]);
+        Ranker4.text = FormatRankText(bestName[3], bestScores[3]);
+        Ranker5.text = FormatRankText(bestName[4], bestScores[4]);
+    }
+
+    private string FormatRankText(string name, float score)
+    {
+        return $"{name} : {score}";
+    }
+
+
+    private void RankingSet(float currentScore, string currentName)
+    {
+        // 1위부터 아래로 검사를 하고 만약에 더 높은 점수가 나오면 반복문 돌리면서 1번에서 찾았다고 하면 아래서부터 땡기기
+        // 현재 점수가 5위 점수보다 높을 시
+        if (currentScore > bestScores[4])
         {
-            rankScore[i] = PlayerPrefs.GetFloat(i.ToString() + "BestScore");
-            rankName[i] = PlayerPrefs.GetString(i.ToString() + "BestName");
+            // 5위를 현재 점수로 바꾼다
+            bestScores[4] = currentScore;
+            for (int i = 4; i > 0; i--)
+            {
+                if (bestScores[i] > bestScores[i - 1])
+                {
+                    bestScores[i - 1] = bestScores[i];
+                }
+            }
+            if (currentScore < bestScores[0])
+            {
+                bestScores[0] = currentScore;
+            }
         }
-        Debug.Log($"랭크{rankName[0]}");
-        Debug.Log($"랭크{rankScore[0]}");
+
+
+        // PlayerPrefs에 저장
+        for (int j = 0; j < 5; j++)
+        {
+            PlayerPrefs.SetFloat(j.ToString() + "BestScore", bestScores[j]);
+            PlayerPrefs.SetString(j.ToString() + "BestName", bestName[j]);
+        }
+
+        UpdateRankText();
     }
 
     public void PlayerNameSet()
     {
-        // TODO :: 이름을 입력했을 때 playerName이 값을 받게 해야함
         playerName = nameInput.text;
         resetBtn.SetActive(true);
         closeBtn.SetActive(true);
@@ -98,11 +97,13 @@ public class RankingManager : MonoBehaviour
 
         if (scoreManager == null)
         {
+            Debug.LogError("ScoreManager instance is null.");
             return;
         }
 
         nowScore = scoreManager.currentScore;
         Debug.Log(nowScore);
+        Debug.Log(playerName);
         RankingSet(nowScore, playerName);
     }
 }
